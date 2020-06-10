a01ec47b52b66018f09f5f52bdac01edbae67017
Mahendra Samarawickrama, PhD <samarawickrama@gmail.com>
2020-06-11 08:25:16 +1000

    Add files via upload

---
+++ nlp_sentiment_analysis.py
@@ -0,0 +1,29 @@
+
+import os
+import csv
+import codecs
+import re
+from string import punctuation
+
+def strip_punctuation(s):
+    return ''.join(c for c in s if c not in punctuation)
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'GCP_NLP_Credentials.json'
+
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+# Instantiates a client
+client = language.LanguageServiceClient()
+
+# Sentiment Analysis
+text = strip_punctuation("Red cross is great!")
+document = types.Document(
+            content=text,
+            type=enums.Document.Type.PLAIN_TEXT)
+
+sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+print(sentiment.score)
